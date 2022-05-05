ad55e587abe9d351693be2c25daf9e3677cca75e
JLPower <laurent5dessy@gmail.com>
2022-05-05 22:23:53 +0200

    commit for patch cart

--- src/reducers/cart.reducer.jsx
+++ src/reducers/cart.reducer.jsx
@@ -8,27 +8,17 @@ const initialState = {
 export default function cartReducer(state = initialState, action) {
     switch (action.type) {
         case ADD_ITEM_CART:
-            const itemFound = false;
-            let cart = []
 
-            cart = state.cart.reduce((cartResult, cartItem) => {
-                // Si l'item courant correspond à item à ajouter, alors on modifie sa quantité
-                if (cartItem.name === action.payload.name) {
-                    itemFound = true
-                    cartItem.quantity++
-                    // On retourne le contenu du panier en plus de l'item courant
-                    return cartResult.push(...cartResult, cartItem)
-                }
-                // On retourne seulement le contenu du panier
-                return cartResult;
-            }, []);
-            // Si l'item n'a pas été trouvé dans le panier, on l'ajoute au tableau
-            if (!itemFound) {
-                cart.push(...action.payload);
+            const find = state.cart.find((item) => item.name === action.payload.name);
+
+            let newCart = state.cart;
+            if (!find) {
+                newCart = [...state.cart,action.payload]
+            }else {
+                find.qtty = find.qtty + action.payload.qtty;
             }
-
             return {
-                cart: cart,
+                cart:newCart,
                 total: state.total + action.payload.price
             }
         default:
