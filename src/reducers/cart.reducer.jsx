d0a305d8c2476accd51957d0e70ff5a476026604
Gaev Cenne <16835349+gaeVG@users.noreply.github.com>
2022-05-05 21:32:52 +0200

    update: fix add item

--- src/reducers/cart.reducer.jsx
+++ src/reducers/cart.reducer.jsx
@@ -8,18 +8,27 @@ const initialState = {
 export default function cartReducer(state = initialState, action) {
     switch (action.type) {
         case ADD_ITEM_CART:
-            console.log(state.cart)
-            return {
+            const itemFound = false;
+            let cart = []
 
-                cart: state.cart.length > 0 ? state.cart.reduce((cartResult,cartItem) => {
-                    if(cartItem.name === action.payload.name) {
-                        cartResult.push({
-                            ...cartItem,
-                            quantity: cartItem.quantity + 1
-                        })
-                    }
-                    return cartResult
-                }, [...state.cart]) : [...state.cart, action.payload],
+            cart = state.cart.reduce((cartResult, cartItem) => {
+                // Si l'item courant correspond à item à ajouter, alors on modifie sa quantité
+                if (cartItem.name === action.payload.name) {
+                    itemFound = true
+                    cartItem.quantity++
+                    // On retourne le contenu du panier en plus de l'item courant
+                    return cartResult.push(...cartResult, cartItem)
+                }
+                // On retourne seulement le contenu du panier
+                return cartResult;
+            }, []);
+            // Si l'item n'a pas été trouvé dans le panier, on l'ajoute au tableau
+            if (!itemFound) {
+                cart.push(...action.payload);
+            }
+
+            return {
+                cart: cart,
                 total: state.total + action.payload.price
             }
         default:
